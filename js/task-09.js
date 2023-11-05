497d563aba607c2678f33988b925440e94f08b3f
mareckkkk <m.walus94@gmail.com>
2023-11-05 11:25:08 +0100

    hw task 09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorButton = document.querySelector("button.change-color");
+
+const background = document.querySelector("body");
+
+const colorSpan = document.querySelector("span.color");
+
+colorButton.addEventListener("click", (e) => {
+  let color = getRandomHexColor();
+  background.style.backgroundColor = color;
+  colorSpan.textContent = color;
+})
