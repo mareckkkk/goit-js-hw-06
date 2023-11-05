09a1be4605993a3602a92d04b056f4723adf20a7
mareckkkk <m.walus94@gmail.com>
2023-11-05 11:19:04 +0100

    hw task 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", submitFuncion);
+
+function submitFuncion(event) {
+	event.preventDefault();
+	const {
+		elements: { email, password },
+	} = event.currentTarget;
+
+	if (email.value === "" || password.value === "") {
+		return alert("please fill in all the fields!");
+	}
+
+	const user = {
+		email: email.value,
+		password: password.value,
+	};
+
+	console.log(user);
+	event.currentTarget.reset();
+}
