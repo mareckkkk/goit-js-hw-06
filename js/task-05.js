8563c11b7ecff8af4e2fe211a4a305670d364494
mareckkkk <m.walus94@gmail.com>
2023-11-05 10:31:34 +0100

    task 05

---
+++ js/task-05.js
@@ -0,0 +1,10 @@
+const textHolder = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
+
+textHolder.addEventListener("input", (e) => {
+	output.textContent = e.target.value;
+
+	if (e.target.value == "") {
+		output.textContent = "Anonymous";
+	}
+});
